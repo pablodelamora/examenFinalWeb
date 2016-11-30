e736b22db79e366302f2fe7fa144e356860f76fd
pablodelamora <pablodelamorav@gmail.com>
2016-11-30 16:45:27 -0600

    asassa

---
+++ startbootstrap-creative-gh-pages/main.js
@@ -0,0 +1,11 @@
+if(localStorage.getItem("session") != ''){
+    $('#loginBtn').hide();
+    $('#logoutBtn').show();
+}else{
+    $('#loginBtn').show();
+    $('#logoutBtn').hide();
+}
+
+function logout(){
+    localStorage.setItem("session", '');
+}
